3c732365e725659f2f01f0f027bf9100bd551b80
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-05-06 17:33:17 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index.between #if wrong input
+  if index.between(0,8) #if wrong input
     return false
   elsif board[index] == nil
     return false #if no input
